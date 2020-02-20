3d8de34db3cdc09feb97c926b7c0c1f6baaf2688
Matheu Plouffe <matheuplouffe@gmail.com>
2020-02-19 23:52:24 -0600

    Added package to try to generate html on server side to generate images.

--- image-generator/image_generation.js
+++ image-generator/image_generation.js
@@ -1,9 +1,26 @@
-const nodeHtmlToImage = require('node-html-to-image')
+const nodeHtmlToImage = require('node-html-to-image');
+const Element = require('html-element');
 
 const generateImage = () => {
+    let htmlNode = {
+        html: {
+            body: {
+                text: "Hello World!"
+            }
+        }
+    };
+    // let document = Element.document;
+    // let htmlNode = document.createElement('html');
+    // let body = document.createElement('body');
+    // body.textContent = "Hello world!";
+
+    // htmlNode.appendChild(body);
+
+    console.log(htmlNode);
+
     nodeHtmlToImage({
     output: './image.png',
-    html: '<html><body>Hello world!</body></html>'
+    html: htmlNode
     })
     .then(() => console.log('The image was created successfully!'))
 }
